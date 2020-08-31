004ad8941c6dafd69c4ede3c87faaca96627dd4d
fengdesheng <2418298596@qq.com>
2020-08-31 15:20:25 +0800

    MasterBean

--- src/main/java/com/example/demo/entity/MasterBean.java
+++ src/main/java/com/example/demo/entity/MasterBean.java
@@ -3,5 +3,7 @@ package com.example.demo.entity;
 public class MasterBean {
     public static void main(String[] args) {
         System.out.println("主分支");
+        System.out.println("主分支");
+        System.out.println("主分支");
     }
 }
